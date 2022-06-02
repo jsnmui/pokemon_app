5faa67f966e91bd054de205b4d56478b8271e432
unknown <jasonmui2004@gmail.com>
2022-06-02 10:52:55 -0400

    changed server.js and Index.ejs

--- server.js
+++ server.js
@@ -63,7 +63,9 @@
 //show views of each pokemon
   app.get("/pokemon/:id", (req, res) => {
           
-   res.render("show", { data: pokemon, index: req.params.id, pageHeader:"Gotta Catch 'Em All" } );
+   res.render("show", {  pageHeader:"Gotta Catch 'Em All", 
+                         data: pokemon[req.params.id] 
+    } );
       
   }); 
 
